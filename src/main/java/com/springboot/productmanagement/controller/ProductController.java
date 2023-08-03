2376ddfb8cdb141b789f6763e2fd84800255f274
dabhisharad13 <dabhisharad13@gmail.com>
2023-08-03 16:06:31 -0700

    Added DTO for Product request

--- src/main/java/com/springboot/productmanagement/controller/ProductController.java
+++ src/main/java/com/springboot/productmanagement/controller/ProductController.java
@@ -1,9 +1,17 @@
 package com.springboot.productmanagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.productmanagement.dto.ProductRequest;
+import com.springboot.productmanagement.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+
+    }
 }
